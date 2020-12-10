1d55b1ae7f0cb00da3851be6791030095b789813
dongwj <dongwj@primeton.com>
2020-12-10 14:27:18 +0800

    测试lint-staged

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,26 @@
  * @Author: 
  * @Date: 2020-12-10 10:25:42
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-10 14:10:37
+ * @LastEditTime: 2020-12-10 14:26:57
 -->
 <template>
   <div class=""></div>
 </template>
 
 <script>
-export default {
-  name: '',
-  components: {},
-  mixins: [],
-  props: {},
-  data() {
-    return {}
-  },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  methods: {},
-}
+  export default {
+    name: '',
+    components: {},
+    mixins: [],
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    methods: {},
+  }
 </script>
 <style lang="scss" scoped></style>
