423dabf1289ddd0873ed27e89ae7ade90774a700
dongwj <dongwj@primeton.com>
2020-12-10 10:36:42 +0800

    提交推动文件

--- push.sh
+++ push.sh
@@ -4,10 +4,14 @@
  # @Author: 
  # @Date: 2020-12-10 10:33:07
  # @LastEditors: dongwenjie
- # @LastEditTime: 2020-12-10 10:34:51
+ # @LastEditTime: 2020-12-10 10:36:11
 ### 
 
-git add ./
-read commitString
-git commit -m commitString
-git push
+  unset msg
+
+  read -p "请输入commit提交的描述: " msg
+
+  git add -A
+  git commit -m $msg
+  git push
+  git status
