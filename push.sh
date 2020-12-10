d703a125b646603c2a2f9e4d7dcba2a93884a410
dongwj <dongwj@primeton.com>
2020-12-10 10:33:49 +0800

    commitString

---
+++ push.sh
@@ -0,0 +1,13 @@
+###
+ # @Descriptions: 
+ # @Version: 
+ # @Author: 
+ # @Date: 2020-12-10 10:33:07
+ # @LastEditors: dongwenjie
+ # @LastEditTime: 2020-12-10 10:33:07
+### 
+
+git add ./
+read commitString
+git commit -m commitString
+git push origin master
