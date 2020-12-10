1757d6006478a1c4fcef8eb3d389484b73bc42ab
dongwj <dongwj@primeton.com>
2020-12-10 14:28:17 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,26 @@
  * @Author: 
  * @Date: 2020-12-10 10:25:42
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-10 14:26:57
+ * @LastEditTime: 2020-12-10 14:27:42
 -->
 <template>
-  <div class=""></div>
+    <div class=""></div>
 </template>
 
 <script>
-  export default {
-    name: '',
-    components: {},
-    mixins: [],
-    props: {},
-    data() {
-      return {}
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    methods: {},
-  }
+    export default {
+        name: '',
+        components: {},
+        mixins: [],
+        props: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        watch: {},
+        created() {},
+        mounted() {},
+        methods: {},
+    }
 </script>
 <style lang="scss" scoped></style>
