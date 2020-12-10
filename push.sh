6f1e84b83bf5e546b82bf06c23b2980912324e10
dongwj <dongwj@primeton.com>
2020-12-10 10:35:03 +0800

    commitString

--- push.sh
+++ push.sh
@@ -4,10 +4,10 @@
  # @Author: 
  # @Date: 2020-12-10 10:33:07
  # @LastEditors: dongwenjie
- # @LastEditTime: 2020-12-10 10:33:07
+ # @LastEditTime: 2020-12-10 10:34:51
 ### 
 
 git add ./
 read commitString
 git commit -m commitString
-git push origin master
+git push
