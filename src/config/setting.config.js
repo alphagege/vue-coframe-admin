49b481e80a2edf0ea11a5d68c6b0c26eeca208c6
dongwj <dongwj@primeton.com>
2020-12-11 15:03:32 +0800

    配置vueconfigjs+7z压缩

--- src/config/setting.config.js
+++ src/config/setting.config.js
@@ -4,10 +4,28 @@
  * @Author:
  * @Date: 2020-12-11 09:21:28
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-11 11:23:14
+ * @LastEditTime: 2020-12-11 15:00:07
  */
 const setting = {
+  // 开发以及部署时的URL
+  publicPath: '',
+  // 生产环境构建文件的目录名
+  outputDir: 'dist',
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: 'static',
+  // 开发环境每次保存时是否输出为eslint编译警告
+  lintOnSave: false,
+  // 标题 （初次加载标题 页面的标题 浏览器的标题）
+  title: 'vue-admin-coframe',
+  // 简写
+  abbreviation: 'coframe',
+  // 开发环境端口号
+  devPort: '9999',
+  // npm run build时是否自动生成7z压缩包
+  build7z: true,
+  // 代码生成机生成在view下的文件夹名称
   templateFolder: 'project',
+  // 是否显示终端donation打印
   donation: true,
 }
 
--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,21 @@
  * @Author:
  * @Date: 2020-12-10 11:14:40
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-11 14:16:22
+ * @LastEditTime: 2020-12-11 15:02:03
  */
 const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
-const { donation } = require('./src/config')
+const {
+  publicPath,
+  assetsDir,
+  outputDir,
+  lintOnSave,
+  title,
+  abbreviation,
+  devPort,
+  build7z,
+  donation,
+} = require('./src/config')
 
 const {
   webpackBarName,
@@ -17,14 +27,37 @@ const {
 } = require('./src/py-layouts')
 if (donation) donationConsole()
 
+const { version, author } = require('./package.json')
+
 const Webpack = require('webpack')
 const WebpackBar = require('webpackbar')
 
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+
 const dayjs = require('dayjs')
+const date = dayjs().format('YYYY_M_D')
 const time = dayjs().format('YYYY-M-D HH:mm:ss')
 
+process.env.VUE_APP_TITLE = title || 'vue-admin-coframe'
+process.env.VUE_APP_AUTHOR = author || 'dongwj@primeton.com'
+process.env.VUE_APP_UPDATE_TIME = time
+process.env.VUE_APP_VERSION = version
+
 module.exports = {
-  lintOnSave: false,
+  publicPath,
+  assetsDir,
+  outputDir,
+  lintOnSave,
+  devServer: {
+    hot: true,
+    port: devPort,
+    open: true,
+    noInfo: false,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
   configureWebpack() {
     return {
       resolve: {
@@ -40,6 +73,10 @@ module.exports = {
     }
   },
   chainWebpack(config) {
+    // 移除 preload 插件
+    config.plugins.delete('preload')
+    // 移除 prefetch 插件
+    config.plugins.delete('prefetch')
     config.when(process.env.NODE_ENV !== 'development', (config) => {
       config.performance.set('hints', false)
       config.devtool('none')
@@ -48,5 +85,27 @@ module.exports = {
         .use(Webpack.BannerPlugin, [`${webpackBanner}${time}`]) // 打包版权声明
         .end()
     })
+
+    if (build7z) {
+      config.when(process.env.NODE_ENV === 'production', (config) => {
+        config
+          .plugin('fileManager')
+          .use(FileManagerPlugin, [
+            {
+              onEnd: {
+                archive: [
+                  {
+                    source: `./${outputDir}`,
+                    destination: `./${outputDir}/${abbreviation}_${outputDir}_${date}.7z`,
+                  },
+                ],
+              },
+            },
+          ])
+          .end()
+      })
+    }
   },
+  runtimeCompiler: true,
+  productionSourceMap: false,
 }
