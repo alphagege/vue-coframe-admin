bea99cd55f4eb87bb76d81fd8d82fa40bb92f5ad
dongwj <dongwj@primeton.com>
2020-12-11 10:55:32 +0800

    3.0

--- plopfile.js
+++ plopfile.js
@@ -4,9 +4,14 @@
  * @Author:
  * @Date: 2020-12-10 14:33:07
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-11 08:34:09
+ * @LastEditTime: 2020-12-11 10:44:36
  */
 const componentGenerator = require('py-templates/component/prompt')
+const viewGenerator = require('py-templates/view/promot')
+const vuexGenerator = require('py-templates/vuex/promot')
+
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator)
+  plop.setGenerator('view', viewGenerator)
+  plop.setGenerator('vuex', vuexGenerator)
 }
---
+++ src/config/setting.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Descriptions:
+ * @Version:
+ * @Author:
+ * @Date: 2020-12-11 09:21:28
+ * @LastEditors: dongwenjie
+ * @LastEditTime: 2020-12-11 09:21:59
+ */
+const setting = {
+  templateFolder: 'project',
+}
