c32e602fdaffa6c9502dc97d48f0e6125c58b2fd
dongwj <dongwj@primeton.com>
2020-12-17 15:51:36 +0800

    环境变量

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@
  * @Author:
  * @Date: 2020-12-10 11:14:40
  * @LastEditors: dongwenjie
- * @LastEditTime: 2020-12-10 11:21:19
+ * @LastEditTime: 2020-12-17 14:25:07
  */
 
 import Vue from 'vue'
@@ -12,7 +12,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/test',
+    component: () => import('../views/test'),
+  },
+]
 const router = new VueRouter({
   routes,
 })
---
+++ src/views/test/index.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Descriptions: 
+ * @Version: 
+ * @Author: 
+ * @Date: 2020-12-17 14:24:02
+ * @LastEditors: dongwenjie
+ * @LastEditTime: 2020-12-17 14:26:35
+-->
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    components: {},
+    mixins: [],
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    created() {
+      console.log(process.env.VUE_APP_EOSKEY)
+    },
+    mounted() {},
+    methods: {},
+  }
+</script>
+<style lang="scss" scoped></style>
